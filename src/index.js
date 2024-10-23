bffed9c1a4a9a220a5bf9a82f54bdd23bf737942
DydyaErl <soyadi6500@gmail.com>
2024-10-23 12:21:54 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -38,9 +38,35 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';  
+
+    
+    for (let i = 0; i < expr.length; i += 10) {
+        let letterCode = expr.slice(i, i + 10);  
+
+        if (letterCode === '**********') {
+            result += ' ';
+        } else {
+            let morse = '';  
+
+            
+            for (let j = 0; j < 10; j += 2) {
+                let pair = letterCode.slice(j, j + 2);
+                if (pair === '10') {
+                    morse += '.';
+                } else if (pair === '11') {
+                    morse += '-';
+                }
+            }
+
+           
+            result += MORSE_TABLE[morse];
+        }
+    }
+
+    return result;  
 }
 
 module.exports = {
     decode
-}
+};
